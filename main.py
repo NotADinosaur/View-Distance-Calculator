5a58dfb8d09a69ed1bf129d84b94401d1622fc22
NotADinosaur <76700400+NotADinosaur@users.noreply.github.com>
2022-04-21 14:03:54 -0700

    very basic units support

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", type = float, help = "input for field of view (in degrees)", required = True)
 parser.add_argument("-r", type = float, help = "radius of the area you want to view", required = True)
-parser.add_argument("-u", type = str, help = "unit to use for radius and distance")
-parser.add_argument("-v", help = "enable verbose output", default = False, action = "store_true")
+parser.add_argument("-v", default = False, action = "store_true", help = "enable verbose output")
+parser.add_argument("-u", type = str, default = "units", help = "unit to use for radius and distance")
 args = parser.parse_args()
 
 def aquire_tan(f):
@@ -14,13 +14,13 @@ def aquire_tan(f):
     
 def aquire_needed_distance(r):
     distance = r / tan
-    print("You need to be " +str(distance) + " units away")
+    print("You need to be " +str(distance) + " " + args.u + " away")
 
 aquire_tan(args.f)
 
 if args.v == True:
     print("FOV: " + str(args.f) + " degrees")
-    print("Radius: " + str(args.r) + "units")
+    print("Radius: " + str(args.r) + " " + args.u)
     print("tan(" + str(args.f) + "): " + str(tan))
 
 aquire_needed_distance(args.r)
