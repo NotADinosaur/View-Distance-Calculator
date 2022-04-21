931a99ddf673e7a7672e7b5d6112b54aebc8f694
NotADinosaur <76700400+NotADinosaur@users.noreply.github.com>
2022-04-21 13:49:03 -0700

    add verbose mode flag

--- main.py
+++ main.py
@@ -4,19 +4,23 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", type = float, help = "input for field of view (in degrees)", required = True)
 parser.add_argument("-r", type = float, help = "radius of the area you want to view", required = True)
+parser.add_argument("-u", type = str, help = "unit to use for radius and distance")
+parser.add_argument("-v", help = "enable verbose output", default = False, action = "store_true")
 args = parser.parse_args()
-
-print("FOV: " + str(args.f) + " degrees")
-print("Radius: " + str(args.r) + " units")
 
 def aquire_tan(f):
     global tan
     tan = math.tan(math.radians(args.f))
-    print(tan)
-
+    
 def aquire_needed_distance(r):
     distance = r / tan
     print("You need to be " +str(distance) + " units away")
 
 aquire_tan(args.f)
+
+if args.v == True:
+    print("FOV: " + str(args.f) + " degrees")
+    print("Radius: " + str(args.r) + "units")
+    print("tan(" + str(args.f) + "): " + str(tan))
+
 aquire_needed_distance(args.r)
